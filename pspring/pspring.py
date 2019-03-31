c82ca8620cb2140254aa43e3c3e71a7e65cc7dc5
Palani <vasudevan.palani@gmail.com>
2019-03-31 10:24:01 -0400

    Added environment checks

--- pspring/pspring.py
+++ pspring/pspring.py
@@ -1,4 +1,11 @@
 context={}
+
+import os
+
+for item in os.environ:
+    if item.startswith("pspring"):
+        os.environ[item.replace("_",".")] = os.environ.get(item)
+
 from .core.bean import *
 from .core.autowired import *
 from .core.context import *
