6f733c362c3814ae10473aa2c4dfd777174e81be
Palani <vasudevan.palani@gmail.com>
2019-04-01 14:38:44 -0400

    updated markdown

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pspring',
-    version='0.0.6',
+    version='0.0.8',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
     url='https://github.com/vasudevan-palani/pspring',
-    long_description="README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['pspring','pspring.core'],
     include_package_data=True,
     description="A framework inspired by spring for python",
